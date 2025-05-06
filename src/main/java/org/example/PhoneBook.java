2b3ece97c66fbfa79285423bf1a3ecbe182dd5d9
Mateusz Milczarek <mateusz.milczarek1@uth.pl>
2025-05-06 16:18:38 +0200

    Drobne poprawki Wersja aplikacji: 1.0.3

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -7,7 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneBook {
-    ArrayList<Contact> phonebook = new ArrayList<>();
+    private ArrayList<Contact> phonebook = new ArrayList<>();
     private String currentFileName = null;
 
     String regex = "^\\d{9}$";
